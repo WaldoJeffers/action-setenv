bdecf8ff78e8d5040d9cc9a804815c2c8b088e12
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2019-08-17 16:03:05 +0200

    remove logs

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,14 @@
 import * as core from '@actions/core'
 
-const stringify = value =>
-  value === undefined ? 'undefined' : JSON.stringify(value)
-
 async function run() {
   try {
     const name = core.getInput('name')
     const value = core.getInput('value')
-    process.stdout.write(`process.env.HOME: ${stringify(process.env.HOME)}`)
-    process.stdout.write(`value: ${stringify(value)}`)
-    const newValue = value.replace(/\$(\w+)/g, (_, envVar) => {
-      process.stdout.write(`envVar: ${stringify(envVar)}`)
-      process.stdout.write(
-        `process.env[envVar]: ${stringify(process.env[envVar])}`,
-      )
-      return process.env[envVar] !== undefined
+    const newValue = value.replace(/\$(\w+)/g, (_, envVar) =>
+      process.env[envVar] !== undefined
         ? (process.env[envVar] as string)
-        : envVar
-    })
-    process.stdout.write(`newValue: ${stringify(newValue)}`)
+        : envVar,
+    )
     core.exportVariable(name, newValue)
   } catch (error) {
     core.setFailed(error.message)
