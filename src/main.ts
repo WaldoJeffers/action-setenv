61d6048b796650265be0b9c5a69fdde8afec7c5f
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2019-08-17 15:50:13 +0200

    handle undefined

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
 import * as core from '@actions/core'
 
+const stringify = value =>
+  value === undefined ? 'undefined' : JSON.stringify(value)
+
 async function run() {
   try {
     const name = core.getInput('name')
     const value = core.getInput('value')
-    process.stdout.write(process.env.HOME as string)
-    process.stdout.write(value)
+    process.stdout.write(`process.env.HOME: ${stringify(process.env.HOME)}`)
+    process.stdout.write(`value: ${stringify(value)}`)
     const newValue = value.replace(/\$(\w+)/g, envVar => {
-      process.stdout.write(envVar)
-      process.stdout.write(process.env[envVar] as string)
+      process.stdout.write(`envVar: ${stringify(envVar)}`)
+      process.stdout.write(
+        `process.env[envVar]: ${stringify(process.env[envVar])}`,
+      )
       return process.env[envVar] !== undefined
         ? (process.env[envVar] as string)
         : envVar
     })
-    process.stdout.write(newValue)
+    process.stdout.write(`newValue: ${stringify(newValue)}`)
     core.exportVariable(name, newValue)
   } catch (error) {
     core.setFailed(error.message)
