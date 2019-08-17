f28643d64b315ca169f150fcf3bc16aa9bacb299
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2019-08-17 15:30:49 +0200

    feat: Handle env variables

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ async function run() {
   try {
     const name = core.getInput('name')
     const value = core.getInput('value')
-    core.debug(`Got ${name}: ${value} as input`)
-    core.exportVariable(name, value)
+    core.exportVariable(
+      name,
+      value.replace(/\$(\w+)/g, envVar =>
+        process.env[envVar] !== undefined
+          ? (process.env[envVar] as string)
+          : envVar,
+      ),
+    )
   } catch (error) {
     core.setFailed(error.message)
   }
